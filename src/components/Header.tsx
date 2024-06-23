c9a47898ca53872cc2aa56d55361d26de8aa0955
bakarykone <bakarykone729@gmail.com>
2024-06-23 23:50:29 +0200

    Header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Header: React.FC = () => {
+  return (
+    <Box textAlign="center">
+      <Typography variant="h4" fontWeight="bold">
+        DÉCOUVREZ <span style={{ color: '#3F5EFB' }}>NOS OFFRES</span>
+      </Typography>
+      <Typography variant="subtitle1" color="textSecondary" mt={2} boxSizing={'border-box'}>
+        Trouvez le plan parfait pour votre entreprise parmi nos différentes options d'abonnement mensuel, trimestriel,
+        semestriel, annuel et biennal.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
